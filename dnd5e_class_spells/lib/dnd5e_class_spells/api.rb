aef8665bb5a424b5123edf154f35a95a4b226e96
Nicholas Merolle <nmerolle11@hotmail.com>
2020-12-18 04:37:17 +0000

    list classes

--- dnd5e_class_spells/lib/dnd5e_class_spells/api.rb
+++ dnd5e_class_spells/lib/dnd5e_class_spells/api.rb
@@ -2,11 +2,16 @@ class  Dnd5eClassSpells::API
 
   BASE_URL = "https://www.dnd5eapi.co/api/"
 
-  def self.get_classes
+  def self.get_klasses
     response = RestClient.get(BASE_URL + "classes")
     data = JSON.parse(response)
-    klass_data = data["results"]
-    binding.pry
+    @klass_data = data["results"]
   end
 
+  def self.list_klasses
+    @klass_data.each_with_index do |klass,index|
+      puts "#{index+1}. #{klass["name"]}"
+    end
+  end
+  
 end
