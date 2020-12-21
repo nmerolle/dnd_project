f00939bf99b3e00a081ee710f906c4b261acf3b8
Nicholas Merolle <nmerolle11@hotmail.com>
2020-12-21 13:30:17 +0000

    added validity test

--- dnd5e_class_spells/lib/dnd5e_class_spells/cli.rb
+++ dnd5e_class_spells/lib/dnd5e_class_spells/cli.rb
@@ -1,7 +1,6 @@
 class Dnd5eClassSpells::CLI
 
   def call
-    
     get_spells
     get_klasses
     get_user_klass_input
@@ -19,11 +18,19 @@ class Dnd5eClassSpells::CLI
   end
 
   def get_user_klass_input
-    puts "\nEnter a class to see info on that class.\n\n"
-    binding.pry
+    puts "\nEnter the index for a class to spells for that class.\n\n"
     @user_klass_input = gets.strip
+    if valid_input(@user_klass_input, Dnd5eClassSpells::Klass.all)
+      puts "x"
+    else
+      puts "\n\nPlease enter a valid index!!"
+      get_user_klass_input
+    end
   end
   
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
+  end
   
 end  
 
